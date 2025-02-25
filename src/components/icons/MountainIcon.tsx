987afa8eb7c2996f1a3ef7a3f383f49ce1af835f
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-25 19:10:09 +0600

    refactor MountainIcon component to update SVG structure and attributes for improved clarity and styling

--- src/components/icons/MountainIcon.tsx
+++ src/components/icons/MountainIcon.tsx
@@ -3,18 +3,25 @@ import { SVGProps } from 'react'
 export default function MountainIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      {...props}
       xmlns='http://www.w3.org/2000/svg'
       width='24'
       height='24'
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      className='lucide lucide-notebook-text'
     >
-      <path d='m8 3 4 8 5-5 5 15H2L8 3z' />
+      <path d='M2 6h4' />
+      <path d='M2 10h4' />
+      <path d='M2 14h4' />
+      <path d='M2 18h4' />
+      <rect width='16' height='20' x='4' y='2' rx='2' />
+      <path d='M9.5 8h5' />
+      <path d='M9.5 12H16' />
+      <path d='M9.5 16H14' />
     </svg>
   )
 }
