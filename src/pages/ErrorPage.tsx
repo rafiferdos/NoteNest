afbfba61e8dd7414828a57ca22b03d0b207df43c
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-25 20:45:32 +0600

    refactor ErrorPage to use Button component for improved styling and user experience

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import { Link } from 'react-router-dom'
 
 export default function ErrorPage() {
@@ -12,12 +13,9 @@ export default function ErrorPage() {
             Looks like you've ventured into the unknown digital realm.
           </p>
         </div>
-        <Link
-          to='/'
-          className='inline-flex h-10 items-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300'
-        >
-          Return to website
-        </Link>
+        <Button variant='default' asChild>
+          <Link to='/'>Return to website</Link>
+        </Button>
       </div>
     </div>
   )
