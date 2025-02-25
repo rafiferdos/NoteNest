de966f601ba0846a707b26d579393195355ce7fe
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-25 20:14:32 +0600

    add AnimatedBackground component and refactor Navbar to utilize it for improved tab navigation

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,8 +5,14 @@ import MountainIcon from '../icons/MountainIcon'
 import MenuIcon from '../icons/MenuIcon'
 import { GlowEffect } from '../ui/glow-effect'
 import { ModeToggle } from '../icons/ModeToggle'
+import { AnimatedBackground } from '../ui/animated-background'
 
 export default function Navbar() {
+  const Tabs = [
+    { name: 'Home', href: '/' },
+    { name: 'Products', href: '/products' },
+    { name: 'About', href: '/about' },
+  ]
   return (
     <header className='flex h-20 w-full shrink-0 items-center px-4 md:px-6'>
       {/* Mobile Menu */}
@@ -24,7 +30,7 @@ export default function Navbar() {
           </Link>
           {/* Mobile Menu Links */}
           <div className='grid gap-2 py-6'>
-            <Link
+            {/* <Link
               to='/products'
               className='flex w-full items-center py-2 text-lg font-semibold'
             >
@@ -35,7 +41,16 @@ export default function Navbar() {
               className='flex w-full items-center py-2 text-lg font-semibold'
             >
               About Us
-            </Link>
+            </Link> */}
+            {Tabs.map((tab) => (
+              <Link
+                key={tab.name}
+                to={tab.href}
+                className='flex w-full items-center py-2 text-lg font-semibold'
+              >
+                {tab.name}
+              </Link>
+            ))}
             <div className='relative'>
               <ModeToggle />
             </div>
@@ -49,15 +64,27 @@ export default function Navbar() {
         <span className='sr-only'>NoteNest</span>
       </Link>
       <nav className='ml-auto hidden lg:flex gap-6 items-center'>
-        <Link to='/' className='...'>
-          Home
-        </Link>
-        <Link to='/products' className='...'>
-          Products
-        </Link>
-        <Link to='/about' className='...'>
-          About
-        </Link>
+        <AnimatedBackground
+          defaultValue={Tabs[0].name}
+          className='rounded-lg bg-yellow-300 dark:bg-yellow-600'
+          transition={{
+            type: 'spring',
+            bounce: 0.2,
+            duration: 0.3,
+          }}
+          enableHover
+        >
+          {Tabs.map((tab) => (
+            <button
+              key={tab.name}
+              data-id={tab.name}
+              type='button'
+              className='px-2 py-0.5 text-zinc-600 transition-colors duration-300 hover:text-zinc-950 dark:text-zinc-400 dark:hover:text-zinc-50'
+            >
+              {tab.name}
+            </button>
+          ))}
+        </AnimatedBackground>
         <div className='relative'>
           <GlowEffect
             colors={['#FF5733', '#33FF57', '#3357FF', '#F1C40F']}
