d10ea130c24ef54d9003db36a772d2a6d1b899a7
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-25 18:07:44 +0600

    update dependencies for lucide-react and add motion package; refactor MainLayout to use a container for layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,11 +3,11 @@ import { Outlet } from 'react-router-dom'
 
 export default function MainLayout() {
   return (
-    <>
+    <div className='container mx-auto max-w-7xl md:w-10/12 w-11/12'>
       <Navbar />
       <div className='min-h-screen'>
         <Outlet />
       </div>
-    </>
+    </div>
   )
 }
