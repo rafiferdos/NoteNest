493b2ee14012d32fbc58a45d9aab835e5406885b
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-25 20:21:17 +0600

    refactor Navbar component to utilize useLocation for active tab highlighting and replace button elements with Link for improved navigation

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 import { Sheet, SheetTrigger, SheetContent } from '@/components/ui/sheet'
 import { Button } from '@/components/ui/button'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import MountainIcon from '../icons/MountainIcon'
 import MenuIcon from '../icons/MenuIcon'
 import { GlowEffect } from '../ui/glow-effect'
@@ -8,6 +8,7 @@ import { ModeToggle } from '../icons/ModeToggle'
 import { AnimatedBackground } from '../ui/animated-background'
 
 export default function Navbar() {
+  const location = useLocation()
   const Tabs = [
     { name: 'Home', href: '/' },
     { name: 'Products', href: '/products' },
@@ -30,23 +31,18 @@ export default function Navbar() {
           </Link>
           {/* Mobile Menu Links */}
           <div className='grid gap-2 py-6'>
-            {/* <Link
-              to='/products'
-              className='flex w-full items-center py-2 text-lg font-semibold'
-            >
-              Products
-            </Link>
-            <Link
-              to='/about'
-              className='flex w-full items-center py-2 text-lg font-semibold'
-            >
-              About Us
-            </Link> */}
             {Tabs.map((tab) => (
               <Link
-                key={tab.name}
                 to={tab.href}
-                className='flex w-full items-center py-2 text-lg font-semibold'
+                key={tab.name}
+                data-id={tab.name}
+                type='button'
+                className={`px-2 py-0.5 transition-colors duration-300 
+    ${
+      location.pathname === tab.href
+        ? 'text-zinc-950 dark:text-zinc-50' // Active state - same as hover
+        : 'text-zinc-600 dark:text-zinc-400 hover:text-zinc-950 dark:hover:text-zinc-50' // Normal state with hover
+    }`}
               >
                 {tab.name}
               </Link>
@@ -75,14 +71,15 @@ export default function Navbar() {
           enableHover
         >
           {Tabs.map((tab) => (
-            <button
+            <Link
+              to={tab.href}
               key={tab.name}
               data-id={tab.name}
               type='button'
               className='px-2 py-0.5 text-zinc-600 transition-colors duration-300 hover:text-zinc-950 dark:text-zinc-400 dark:hover:text-zinc-50'
             >
               {tab.name}
-            </button>
+            </Link>
           ))}
         </AnimatedBackground>
         <div className='relative'>
