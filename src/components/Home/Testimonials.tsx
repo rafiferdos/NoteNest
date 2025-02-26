509154b98d33b02dc699f01c87b1f1bd9fab0d3f
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-27 00:40:40 +0600

    refactor Testimonials component; adjust padding for improved layout

--- src/components/Home/Testimonials.tsx
+++ src/components/Home/Testimonials.tsx
@@ -5,7 +5,7 @@ import avatar3 from '../../assets/avatar3-test.png'
 
 export default function Testimonials() {
   return (
-    <section className='w-full py-12 md:py-24 lg:py-32 flex items-center justify-center'>
+    <section className='w-full py-7 md:py-12 lg:py-16 flex items-center justify-center'>
       <div className='container grid max-w-5xl items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10'>
         <div className='space-y-3'>
           <h2 className='text-3xl font-thin tracking-tighter sm:text-4xl md:text-5xl'>
