41fb485e1256a56e8ae8f83eb80bec38793b8e3e
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-26 12:52:17 +0600

    add FeaturedProducts component to Home page and update layout

---
+++ src/components/Home/FeaturedProducts.tsx
@@ -0,0 +1,14 @@
+const FeaturedProducts = () => {
+  return (
+    <>
+      <h1 className='text-center text-2xl md:text-3xl'>
+        <span className='font-charm text-3xl md:text-4xl'>DESIGNED </span>
+        by us, {' '}
+        <span className='font-charm text-3xl md:text-4xl'>PERSONALIZED </span>
+        by you.
+      </h1>
+    </>
+  )
+}
+
+export default FeaturedProducts
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
-import Banner from "@/components/Home/Banner"
+import Banner from '@/components/Home/Banner'
+import FeaturedProducts from '@/components/Home/FeaturedProducts'
 
 const Home = () => {
   return (
-    <>
+    <div className='lg:space-y-16 space-y-8'>
       <Banner />
-    </>
+      <FeaturedProducts />
+    </div>
   )
 }
 
