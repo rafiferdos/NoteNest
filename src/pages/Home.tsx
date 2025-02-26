68ddec3df05a83f00135dd5b334d717ee4d6409b
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-26 11:57:49 +0600

    add Banner component with animated TextLoop and update Home page; include custom font and styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import Banner from "@/components/Home/Banner"
+
 const Home = () => {
   return (
-    <div className='flex items-center justify-center'>
-      <h1 className='text-2xl font-bold'>Welcome to NoteNest</h1>
-    </div>
+    <>
+      <Banner />
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -50,7 +50,10 @@ export default {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			}
-  		}
+			},
+			fontFamily: {
+				charm: ['charm', 'cursive'],
+			}
   	}
   },
   plugins: [require("tailwindcss-animate")],
