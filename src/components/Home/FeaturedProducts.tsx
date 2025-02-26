26f65b64f73bc5a406544a9458f0d944789bf537
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-26 13:15:10 +0600

    add Testimonials component to Home page and include Avatar support; update FeaturedProducts layout

--- src/components/Home/FeaturedProducts.tsx
+++ src/components/Home/FeaturedProducts.tsx
@@ -1,12 +1,18 @@
 const FeaturedProducts = () => {
   return (
     <>
-      <h1 className='text-center text-2xl md:text-3xl'>
-        <span className='font-charm text-3xl md:text-4xl'>DESIGNED </span>
-        by us, {' '}
-        <span className='font-charm text-3xl md:text-4xl'>PERSONALIZED </span>
-        by you.
-      </h1>
+      <div className="space-y-8">
+        <div>
+          <h1 className='text-center text-2xl md:text-3xl font-thin'>
+            <span className='font-charm text-3xl md:text-4xl'>DESIGNED </span>
+            by us, {' '}
+            <span className='font-charm text-3xl md:text-4xl'>
+              PERSONALIZED{' '}
+            </span>
+            by you.
+          </h1>
+        </div>
+      </div>
     </>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Banner from '@/components/Home/Banner'
 import FeaturedProducts from '@/components/Home/FeaturedProducts'
+import Testimonials from '@/components/Home/Testimonials'
 
 const Home = () => {
   return (
     <div className='lg:space-y-16 space-y-8'>
       <Banner />
       <FeaturedProducts />
+      <Testimonials />
     </div>
   )
 }
