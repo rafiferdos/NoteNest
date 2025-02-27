89745989040f728ed29098c240534d60efe9417d
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-27 21:27:44 +0600

    fix: add error image to ErrorPage for better user experience

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,10 +1,14 @@
 import { Button } from '@/components/ui/button'
 import { Link } from 'react-router-dom'
+import errorImg from '@/assets/404.png'
 
 export default function ErrorPage() {
   return (
     <div className='flex items-center min-h-screen px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16'>
       <div className='w-full space-y-6 text-center'>
+        <div className='max-w-xs mx-auto'>
+          <img src={errorImg} alt='' />
+        </div>
         <div className='space-y-3'>
           <h1 className='text-4xl font-bold tracking-tighter sm:text-5xl'>
             Oops! Lost in Cyberspace
