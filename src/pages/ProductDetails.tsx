f4ff1438cd9ba2d858dd7c51a30cbf80b072e1ba
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-27 21:45:56 +0600

    fix: update ProductDetails route parameter for consistency and improve code readability

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ProductDetails: React.FC = () => {
   return (
-    <div>
+    <>
       
-    </div>
+    </>
   );
 };
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
         element: <About />,
       },
       {
-        path: 'products/:id',
+        path: 'products/:productId',
         element: <ProductDetails />,
       },
     ],
